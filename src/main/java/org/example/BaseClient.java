2c65c15e18d8903aa12f994bb7bcfb49ee84d55e
irina-maleeva <iwmaleeva@gmail.com>
2023-09-10 12:06:10 +0300

    added BaseClient and pom

---
+++ src/main/java/org/example/BaseClient.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseClient {
+    private static final String BASE_URL = "https://stellarburgers.nomoreparties.site/api/";
+
+    protected RequestSpecification getBaseSpec() {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
